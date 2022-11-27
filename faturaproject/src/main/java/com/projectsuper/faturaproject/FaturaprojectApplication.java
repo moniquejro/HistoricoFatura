9b2af804e5cf44578c25dd6a5f9b5db1be4ef709
Monique Oliveira <moniquejroliveira@gmail.com>
2022-11-26 21:46:18 -0300

    commit spring

---
+++ faturaproject/src/main/java/com/projectsuper/faturaproject/FaturaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.projectsuper.faturaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaprojectApplication.class, args);
+	}
+
+}
---
+++ faturaproject/src/test/java/com/projectsuper/faturaproject/FaturaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsuper.faturaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
